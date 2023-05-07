f1a71f8e7692ecbdb99ab17d54f46b4b6f1808c4
Andy12345671234567 <84699758+Andy12345671234567@users.noreply.github.com>
2023-05-07 18:50:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.VAPOR])
 server = app.server
 
 # import file
-file_name = "/Andy12345671234567/test/blob/1af4a3b290884c06f232a7f44780396d9c19399d/train.csv"
+file_name = "https://raw.githubusercontent.com/Andy12345671234567/test/103c303a39eb0e63b621113a7ec02e4cd694d529/train.csv"
 df = pd.read_csv(file_name)
 
 
